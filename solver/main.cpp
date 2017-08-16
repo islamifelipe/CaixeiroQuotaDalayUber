68f3533bfb77a238b7e16e1cec80882bfb6619a0
Islame Felipe Da costa Fernandes <islame-felipe.da-costa-fernandes@etu.unice.fr>
2017-08-16 18:59:37 -0300

    TSP funcionando

--- solver/main.cpp
+++ solver/main.cpp
@@ -29,6 +29,9 @@ int main(){
 	int R; // capacidade de carro
 	double K; // quota minima a ser coletada pelo caixeiro
 
+
+	//////////////////// LEITURA DA INSTÂNCIA ////////////////////
+
 	cin>>n;
 	cin>>l;
 	cin>>R;
@@ -38,6 +41,7 @@ int main(){
 	double **L = new double*[l]; // matriz lx4 de passageiros: tarifa maxxima, tempo maximo, origem e destino
 	double **penalidades = new double*[l]; // matriz lxn de penalidades: penalidade[i][j] = penalidade de desembarcar i em j
 	double **vertices = new double*[n]; // matriz nx2 do bonus e delay de cada vértice 
+
 
 	for (int i=0; i<n; i++){ // inicializa e ler custos das arestas
 		custos[i] = new double[n];
@@ -76,116 +80,117 @@ int main(){
 		}
 	}
 
-	// /// TODO: precisa ser revisado TSP
-	// GRBEnv env = GRBEnv();;
- // 	env.set("OutputFlag","0");
-	// //env.set(GRB_DoubleParam_IterationLimit,MAXITERACOES);
- // 	//env.set(GRB_DoubleParam_Heuristics, 0); // 0% do tempo aplicado a heuristica do root
-  
+	//////////////////// FIM DA LEITURA DA INSTÂNCIA ////////////////////
 
- //  	//env.set(GRB_IntParam_NodeMethod, 0);
- // 	//env.set(GRB_IntParam_Presolve, 0);
- //  	env.set(GRB_IntParam_Method, 1); // 1 é mais rapido //primal
- //  	env.set(GRB_IntParam_SiftMethod,1);
-	// GRBModel model = GRBModel(env);;
+	//////////////////// CONSTRUINDO MODELO ////////////////////
 
-	// //inicializa variaveis;
-	// for (int i=0; i<g->getQuantArestas(); i++){ // se o grafo for direcionado, exitira uma aresta ij e outra ji
-	// 	int origem = g->getArestas(i)->getOrigem();
-	// 	int destino = g->getArestas(i)->getDestino();
-	// 	x[origem][destino] = model.addVar(0.0, 1, 0.0, GRB_CONTINUOUS, "x"+std::to_string(origem)+std::to_string(destino)); // relaxacao linear
-	// 	x[destino][origem] = model.addVar(0.0, 1, 0.0, GRB_CONTINUOUS, "x"+std::to_string(destino)+std::to_string(origem));
-	// }///GRB_BINARY
-	// for (int i=1; i<g->getQuantVertices(); i++){
-	// 	u[i] = model.addVar(0.0, 10000000, 0.0, GRB_CONTINUOUS, "u"+std::to_string(i));
-	// }
+	GRBEnv env = GRBEnv();; 
+ 	env.set("OutputFlag","0"); // desliga o output do solver
+ 	GRBModel model = GRBModel(env);; // cria modelo
 
- //   model.update();
- //   int constrCont = 0;
- //   GRBLinExpr exprObjet;
- //   GRBLinExpr rest1,nova;
- //   double um = 1;
- //   for (int i=0; i<g->getQuantArestas(); i++){
- //   		double peso = g->getArestas(i)->getPeso();
- //   		exprObjet.addTerms(&peso, &x[g->getArestas(i)->getOrigem()][g->getArestas(i)->getDestino()],1); 
- //   		exprObjet.addTerms(&peso, &x[g->getArestas(i)->getDestino()][g->getArestas(i)->getOrigem()],1); //destino origem também ??????
+ 	env.set(GRB_IntParam_Method, 1); // 1 é mais rapido //primal
+  	env.set(GRB_IntParam_SiftMethod,1);
+
+	/****************** VARIAVEIS DO MODELO ******************/
+
+	GRBVar *u; //usada pelas restricoes de subcilo
+	GRBVar **x; //diz se o caixeiro passou pela aresta ij
+
+	x = new GRBVar*[n]; 
+   	u = new GRBVar[n];
+
+   	for (int i=0; i<n;i++){
+        x[i] = new GRBVar[n];
+   	}
+
+	/****************** FUNCAO OBJETIVO E RESTRICOES DO MODELO ******************/
+
+   	for (int i=0; i<n; i++){ // se o grafo for direcionado, exitira uma aresta ij e outra ji
+		for (int j=i+1; j<n; j++){
+			x[i][j] = model.addVar(0.0, 1, 0.0, GRB_BINARY, "x"+std::to_string(i)+std::to_string(j)); 
+			x[j][i] = model.addVar(0.0, 1, 0.0, GRB_BINARY, "x"+std::to_string(j)+std::to_string(i));
+		}
+	}
+
+	for (int i=1; i<n; i++){
+		u[i] = model.addVar(0.0, 10000000, 0.0, GRB_CONTINUOUS, "u"+std::to_string(i));
+	}
+
+	model.update();
+   
+   	int constrCont = 0;
+
+    GRBLinExpr exprObjet;
+    GRBLinExpr rest1,nova;
+    double um = 1;
+    for (int i=0; i<n; i++){
+    	for (int j=i+1; j<n; j++){
+    		exprObjet.addTerms(&custos[i][j], &x[i][j],1);  // sentido direto (conceitual) da aresta
+   			exprObjet.addTerms(&custos[i][j], &x[j][i],1); // sentido inverso (conceitual) da aresta
    		
- //      if (g->getArestas(i)->getDestino()==0 || g->getArestas(i)->getOrigem()==0){
- //   			rest1.addTerms(&um, &x[g->getArestas(i)->getOrigem()][g->getArestas(i)->getDestino()],1);
- //   		//	rest1.addTerms(&um, &x[g->getArestas(i)->getDestino()][g->getArestas(i)->getOrigem()],1);
+    	}
+   	}
+   	model.setObjective(exprObjet,GRB_MINIMIZE); 
+   	for (int i=1; i<n; i++){
+   		rest1.addTerms(&um, &x[i][0],1); 
    		
- //      }
- //   }
- 
- //   model.setObjective(exprObjet,GRB_MINIMIZE); 
- //  model.addConstr(rest1, GRB_EQUAL, 1,std::to_string(constrCont++));
- //   //model.update();
+   	}
+  	model.addConstr(rest1, GRB_EQUAL, 1,std::to_string(constrCont++));
+	  
+  	for (int j=1; j<n; j++){ // para todo j
+  		GRBLinExpr rest2;
+  		for (int i=0; i<n; i++){
+	        if (i!=j){ 
+	         	rest2.addTerms(&um,&x[j][i],1);
+	        }
+      	}
+      	model.addConstr(rest2, GRB_EQUAL, 1,std::to_string(constrCont++));
+	}
 
- //   for (int j=1; j<g->getQuantVertices(); j++){
- //  		GRBLinExpr rest2;
- //  		for (int i=0; i<g->getQuantVertices(); i++){
- //        if (i!=j){ 
- //          if (g->isAdjacente(j,i)!=-1){// sai de j
- //            rest2.addTerms(&um,&x[j][i],1);
- //          } 
- //        }
- //      }
- //      model.addConstr(rest2, GRB_EQUAL, 1,std::to_string(constrCont++));
+	for (int i=1; i<n; i++){ // para todo i
+    	GRBLinExpr rest3;
+  		for (int j=0; j<n; j++){
+    		if (i!=j){ 
+    			rest3.addTerms(&um,&x[j][i],1); 
+    		}
+  		}
+       	model.addConstr(rest3, GRB_EQUAL, 1,std::to_string(constrCont++));
+    }
 
- //    }
- //    //cout<<"\n\n"<<endl;
- //    for (int i=1; i<g->getQuantVertices(); i++){
- //      GRBLinExpr rest3;
- //      for (int j=0; j<g->getQuantVertices(); j++){
- //        if (i!=j){ 
- //          if (g->isAdjacente(j,i)!=-1){// sai de j
- //            rest3.addTerms(&um,&x[j][i],1);
- //           // cout<<"+1x"<<j<<i<<" ";
- //          } 
- //        }
- //      }
- //       model.addConstr(rest3, GRB_EQUAL, 1,std::to_string(constrCont++));
-     
- //    }
-  	
- //   double menos1=-1;
- //   double p = g->getQuantVertices()-1;
- //    for (int i=1; i<g->getQuantVertices(); i++){
- //    	 for (int j=1; j<g->getQuantVertices(); j++){
- //    	 	if(i!=j && g->isAdjacente(i,j)!=-1){
- //    	 		GRBLinExpr rest4;
- //          //cout<<"+ "<<um<<"u"<<i<<" ";
- //    			rest4.addTerms(&um,&u[i],1);
- //    	 		rest4.addTerms(&menos1,&u[j],1);
- //         // cout<<"- 1"<<"u"<<j<<" ";
- //    	 		rest4.addTerms(&p,&x[i][j],1); // grafo nao completo
- //         // cout<<"+ "<<p<<"x"<<i<<j<<" ";
- //    	 		model.addConstr(rest4, GRB_LESS_EQUAL, p-1,std::to_string(constrCont++));
- //  			  //cout<<"<= "<<p-1<<endl;
- //          }
- //    	 }
- //    }
- //     model.optimize();
- //      // model.update();
-	// int optimstatus = model.get(GRB_IntAttr_Status);
-	// vector<Aresta*> caminho;
-	// Informacao ret = {caminho,0};
-	// if (optimstatus != GRB_INFEASIBLE){
-	// 	for (int i=0; i<g->getQuantArestas(); i++){
-	// 		try{
- //        if (x[g->getArestas(i)->getDestino()][g->getArestas(i)->getOrigem()].get(GRB_DoubleAttr_X)>=0.1 || x[g->getArestas(i)->getOrigem()][g->getArestas(i)->getDestino()].get(GRB_DoubleAttr_X)>=0.1){
- //         //  cout<<"x"<<g->getArestas(i)->getOrigem()<<"_"<<g->getArestas(i)->getDestino()<<" = "<<x[g->getArestas(i)->getOrigem()][g->getArestas(i)->getDestino()].get(GRB_DoubleAttr_X)<<endl;
- //         // cout<<"x"<<g->getArestas(i)->getDestino()<<"_"<<g->getArestas(i)->getOrigem()<<" = "<<x[g->getArestas(i)->getDestino()][g->getArestas(i)->getOrigem()].get(GRB_DoubleAttr_X)<<endl;
-          
- //          ret.caminho.push_back(g->getArestas(i));
- //          ret.custo+=g->getArestas(i)->getPeso();
- //        }
- //      } catch(GRBException ex){
 
- //      }
-	// 	}
-	// } 
+    double menos1=-1;
+    double p = n-1;
+    for (int i=1; i<n; i++){
+    	for (int j=1; j<n; j++){
+    	 	if(i!=j){
+    	 		GRBLinExpr rest4;
+    			rest4.addTerms(&um,&u[i],1);
+    	 		rest4.addTerms(&menos1,&u[j],1);
+    	 		rest4.addTerms(&p,&x[i][j],1); // grafo nao completo
+         		model.addConstr(rest4, GRB_LESS_EQUAL, p-1,std::to_string(constrCont++));
+  			 
+          	}
+    	}
+    }
 
-	// return ret;
+	//////////////////// FIM DA CONSTRUÇAO DO MODELO ////////////////////
+
+    model.optimize();
+
+    int optimstatus = model.get(GRB_IntAttr_Status);
+	if (optimstatus != GRB_INFEASIBLE){
+		for (int i=0; i<n; i++){
+			for (int j=i+1; j<n; j++){
+				try{
+	        		if (x[j][i].get(GRB_DoubleAttr_X)==1 || x[i][j].get(GRB_DoubleAttr_X)==1){
+	        			cout<<"("<<i<<","<<j<<") ";
+	        		}
+        		} catch(GRBException ex){
+
+      			}
+			}
+		}
+	}
+    cout<<endl;
 	return 0;
 }
